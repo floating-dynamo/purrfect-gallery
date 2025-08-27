0f40d8309bbcf283dd27db6dd01769efb475055d
Sridhar Maskeri <shridharmaskeri@gmail.com>
2025-08-27 19:14:40 +0530

    Fixed mock response issue for fetching cats

--- src/services/api/mock-api-service.ts
+++ src/services/api/mock-api-service.ts
@@ -1,20 +1,27 @@
-import { ApiService, CatListItem, FetchCatDetailsResponse } from '@/lib/types';
+import {
+  ApiService,
+  FetchCatDetailsResponse,
+  FetchCatsResponse,
+} from '@/lib/types';
 import { MOCK_FETCH_CATS_REPSONSE } from '../../features/cats/mock';
 
-const fetchCatsResponse: CatListItem[] = JSON.parse(
+const fetchCatsResponse = JSON.parse(
   JSON.stringify(MOCK_FETCH_CATS_REPSONSE)
-);
+) as FetchCatsResponse;
 
 const mockApiService: ApiService = {
   async fetchCats({ limit }) {
     return new Promise((resolve) => {
       setTimeout(() => {
-        resolve(fetchCatsResponse.slice(0, limit));
+        resolve({
+          cats: fetchCatsResponse.cats.slice(0, limit),
+          paginationCount: fetchCatsResponse.paginationCount,
+        });
       }, 1000);
     });
   },
   async fetchCatById({ id }) {
-    const cat = fetchCatsResponse.find((cat) => cat.id === id);
+    const cat = fetchCatsResponse.cats.find((cat) => cat.id === id);
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         if (cat) {
