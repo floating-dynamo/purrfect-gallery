531f4359474aadeeafc4612766799b0b516e0482
Sridhar Maskeri <shridharmaskeri@gmail.com>
2025-09-01 10:27:45 +0530

    Updated Navbar for mobile responsiveness

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,25 +1,67 @@
-import { PawPrint } from 'lucide-react';
+'use client';
+import { MenuIcon, PawPrint } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
+import { Button } from './ui/button';
+import { cn } from '@/lib/utils';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between px-8 py-4 font-sans items-center tracking-tight border-b-2 mb-4">
-      <h1 className="font-bold text-lg sm:text-2xl flex gap-2 text-primary">
-        {'Purrfect Gallery'} <PawPrint className="size-4 sm:size-6" />
-      </h1>
-      {/* <div className="hidden md:flex gap-4 text-sm sm:text-lg"> */}
-      <div className="flex gap-4 text-sm sm:text-lg">
-        <Link href={'/'}>Home</Link>
-        <Link href={'/cats/favourites'}>Favourites</Link>
-        <Link href={'/cats/compare-breeds'}>Compare Breeds</Link>
+      <Link
+        href="/"
+        className="font-bold text-lg sm:text-2xl flex gap-2 text-primary"
+      >
+        Purrfect Gallery <PawPrint className="size-4 sm:size-6" />
+      </Link>
+
+      {/* Desktop Nav */}
+      <NavigationLinks className="hidden md:flex" />
+
+      {/* Mobile Nav */}
+      <div className="md:hidden">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button size="icon" variant="ghost" aria-label="Toggle menu" className='cursor-pointer'>
+              <MenuIcon className="size-5" />
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            align="end"
+            sideOffset={8}
+            className="flex flex-col gap-2 p-3 w-40"
+          >
+            <NavigationLinks className="flex flex-col gap-2" />
+          </PopoverContent>
+        </Popover>
       </div>
-      {/* <div className="flex md:hidden">
-        <Button size={'icon'} className="cursor-pointer">
-          <MenuIcon className="size-5" />
-        </Button>
-      </div> */}
     </nav>
+  );
+};
+
+const NavigationLinks = ({ className }: { className?: string }) => {
+  return (
+    <ul className={cn('gap-4 text-sm sm:text-lg flex', className)}>
+      {[
+        { href: '/', label: 'Home' },
+        { href: '/cats/favourites', label: 'Favourites' },
+        { href: '/cats/compare-breeds', label: 'Compare Breeds' },
+      ].map(({ href, label }) => (
+        <li key={href}>
+          <Link
+            href={href}
+            className="hover:text-primary font-medium transition-colors font-sans"
+          >
+            {label}
+          </Link>
+        </li>
+      ))}
+    </ul>
   );
 };
 
